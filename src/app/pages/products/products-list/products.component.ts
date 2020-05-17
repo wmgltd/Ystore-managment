bc8e32bfe73d71e4938aae46f508f86152b94c97
gittit123 <gitit@rachip.com>
2020-05-17 10:49:14 +0300

    -fix img src

--- src/app/pages/products/products-list/products.component.ts
+++ src/app/pages/products/products-list/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/services/products.service';
 import { Product } from 'src/app/models/product';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-products',
@@ -11,6 +12,7 @@ export class ProductsComponent implements OnInit {
   data: Product[] = [];
   displayedColumns: string[] = ['status', 'id', 'img', 'name', 'description', 'english_name', 'english_description', 'price', 'sale_price', 'stock_units', 'open'];
   isLoadingResults = true;
+  imgUrl=environment.imgUrl;
   constructor(private productsService :ProductsService) { }
 
   ngOnInit(): void {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: '../Ystore-server/api/'
+  baseUrl: '../Ystore-server/api/',
+  imgUrl:"../Ystore-server/"
 };
